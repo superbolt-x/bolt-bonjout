bafc21017993f350c6e66a0024e0ae9792e73146
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2024-03-20 10:37:58 -0400

    Update blended_performance.sql
    adding campaign name and region

--- models/reporting/blended_performance.sql
+++ models/reporting/blended_performance.sql
@@ -5,7 +5,7 @@
 {% set date_granularity_list = ['day', 'week', 'month', 'quarter', 'year'] %}
     
 With meta as (
-select date, date_granularity, 'Meta' as channel, SUM(coalesce(spend,0)) as spend, SUM(coalesce(purchases,0)) as paid_purchase,
+select date, date_granularity, campaign_name,'' as region, 'Meta' as channel, SUM(coalesce(spend,0)) as spend, SUM(coalesce(purchases,0)) as paid_purchase,
 SUM(coalesce(revenue,0)) as paid_revenue,
 SUM(coalesce(link_clicks,0)) as clicks,
 SUM(coalesce(impressions,0)) as impressions,
@@ -13,9 +13,13 @@ SUM(coalesce(impressions,0)) as impressions,
 0 as shopify_first_orders,
 0 as shopify_revenue
 from {{ source('reporting', 'facebook_ad_performance') }}
-group by 1,2),
+group by 1,2,3),
 
-google as (select date, date_granularity, 'Google' as channel, SUM(coalesce(spend,0)) as spend, SUM(coalesce(purchases,0)) as paid_purchase,
+google as (select date, date_granularity,campaign_name, 
+'' as region, 
+'Google' as channel, 
+SUM(coalesce(spend,0)) as spend, 
+SUM(coalesce(purchases,0)) as paid_purchase,
 SUM(coalesce(revenue,0)) as paid_revenue,
 SUM(coalesce(clicks,0)) as clicks,
 SUM(coalesce(impressions,0)) as impressions,
@@ -23,11 +27,15 @@ SUM(coalesce(impressions,0)) as impressions,
 0 as shopify_first_orders,
 0 as shopify_revenue
 from {{ source('reporting', 'googleads_campaign_performance') }}
-group by 1,2),
+group by 1,2,3),
 
 shopify as (
 {%- for date_granularity in date_granularity_list %}
-select DATE_TRUNC('{{date_granularity}}' ,date::date) as date, '{{date_granularity}}' as date_granularity, 'Shopify' as channel, 
+select DATE_TRUNC('{{date_granularity}}' ,date::date) as date, 
+'{{date_granularity}}' as date_granularity, 
+'' as campaign_name,
+region,
+'Shopify' as channel, 
 0 as spend,
 0 as paid_purchase,
 0 as paid_revenue,
@@ -37,7 +45,7 @@ sum(coalesce(orders,0)) as shopify_purchase,
 sum(coalesce(first_orders,0)) as shopify_first_orders,
 sum(coalesce(total_sales,0)) as shopify_revenue
 from {{ source('reporting', 'shopify_sales_region') }}
-group by 1,2
+group by 1,2,3
     {% if not loop.last %}UNION ALL
     {% endif %}
 {% endfor %}
